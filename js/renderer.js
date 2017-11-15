e3fb00d975c1ff56f17b90424e7aed6bfe4fd47b
jobtalle <jobtalle@hotmail.com>
2017-11-15 22:44:42 +0100

    Renderer is now a class

--- js/renderer.js
+++ js/renderer.js
@@ -1,33 +1,41 @@
-var camera, scene, renderer;
-var geometry, material, mesh;
-
-function initialize() {
-	var canvas =  document.getElementById("renderer");
-
-    camera = new THREE.PerspectiveCamera( 70, canvas.offsetWidth / canvas.offsetHeight, 0.01, 10 );
-    camera.position.z = 1;
-
-    scene = new THREE.Scene();
-
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-    material = new THREE.MeshNormalMaterial();
-
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
-
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( canvas.offsetWidth, canvas.offsetHeight );
-	canvas.appendChild( renderer.domElement );
+function Renderer(element) {
+	this.width = element.offsetWidth;
+	this.height = element.offsetHeight;
 	
-	animate();
+	this.initializeCamera();
+	this.initializeRenderer(element);
+	this.initializeScene();
+	this.animate();
 }
 
-function animate() {
-
-    requestAnimationFrame( animate );
-
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
-
-    renderer.render( scene, camera );
+Renderer.prototype = {
+	getMesh() {
+		var geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+		var material = new THREE.MeshNormalMaterial();
+	
+		return new THREE.Mesh(geometry, material);
+	},
+	
+	initializeCamera() {
+		this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10);
+		this.camera.position.z = 1;
+	},
+	
+	initializeRenderer(element) {
+		this.renderer = new THREE.WebGLRenderer({antialias: true});
+		this.renderer.setSize(this.width, this.height);
+		
+		element.appendChild(this.renderer.domElement);
+	},
+	
+	initializeScene() {
+		this.scene = new THREE.Scene();
+		this.scene.add(this.getMesh());
+	},
+	
+	animate() {
+		requestAnimationFrame(this.animate.bind(this));
+		
+		this.renderer.render(this.scene, this.camera);
+	}
 }
