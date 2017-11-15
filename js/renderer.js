0eeba28bb02c271939979941e92207017eebf6e6
Job <jobtalle@hotmail.com>
2017-11-15 15:37:07 +0100

    Added three.js renderer

--- js/renderer.js
+++ js/renderer.js
@@ -1,3 +1,33 @@
+var camera, scene, renderer;
+var geometry, material, mesh;
+
 function initialize() {
+	var canvas =  document.getElementById("renderer");
+
+    camera = new THREE.PerspectiveCamera( 70, canvas.offsetWidth / canvas.offsetHeight, 0.01, 10 );
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    material = new THREE.MeshNormalMaterial();
+
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( canvas.offsetWidth, canvas.offsetHeight );
+	canvas.appendChild( renderer.domElement );
 	
+	animate();
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render( scene, camera );
 }
