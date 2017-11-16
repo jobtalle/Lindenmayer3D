17ae7147b04b88bacb00eb6fb1afed2a6c5e2fe1
Job <jobtalle@hotmail.com>
2017-11-16 16:11:57 +0100

    Implemented rule parsing

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,12 @@ function Controller(element) {
 	
 	var system = new Lindenmayer();
 	
-	system.addRule("A(x) = A(x + 1)");
-	console.log(system.process("A(0)B(3, 4, 5)A", 1));
+	system.addRule("A(x) : x < 3 = A(x + 1)");
+	
+	console.log("Rules:");
+	for(var rule = 0; rule < system.rules.length; ++rule)
+		console.log(system.rules[rule]);
+	
+	console.log("Result:");
+	console.log(Lindenmayer.prototype.toString(system.process("A(0)B(3, 4, 5)A", 1)));
 }
--- js/lindenmayer.js
+++ js/lindenmayer.js
@@ -1,15 +1,77 @@
-function Rule(string) {
-	var source = string.replace(/\s/g, "");
-	var lhs = source.substring(0, source.lastIndexOf("="));
-	var rhs = source.substring(source.lastIndexOf("=") + 1);
+function RuleHead(string) {
+	var index = 0;
+	var predecessorIndex = string.indexOf("<");
+	var successorIndex = string.indexOf(">");
 	
-	console.log(lhs);
-	console.log(rhs);
+	if(predecessorIndex != -1) {
+		this.predecessor = new Symbol(string, index);
+		
+		index = predecessorIndex + 1;
+	}
+	else {
+		this.predecessor = null;
+	}
+	
+	this.symbol = new Symbol(string, index);
+	
+	if(successorIndex != -1)
+		this.successor = new Symbol(string, successorIndex + 1);
+	else
+		this.successor = null;
+	
+	this.condition = string.substr(string.indexOf(":") + 1);
 }
 
-function Symbol(symbol, parameters) {
-	this.symbol = symbol;
-	this.parameters = parameters;
+function RuleBody(string) {
+	this.body = Lindenmayer.prototype.toSymbols(string);
+}
+
+function Rule(string) {
+	var source = string.replace(/\s/g, "");
+	
+	this.head = new RuleHead(source.substring(0, source.lastIndexOf("=")));
+	this.body = new RuleBody(source.substring(source.lastIndexOf("=") + 1));
+}
+
+function Symbol(string, index) {
+	this.parse(string, index);
+}
+
+Symbol.prototype = {
+	parse(string, index) {
+		var startIndex = index;
+		
+		this.symbol = string[index];
+		
+		if(index + 1 < string.length && string[index + 1] == "(") {
+			var start = ++index + 1;
+			while(string[++index] != ")");
+			
+			this.parameters = string.substr(start, index - start).split(",");
+		}
+		
+		this.length = index - startIndex;
+	},
+	
+	getArity() {
+		return this.parameters.length;
+	},
+	
+	print() {
+		var str = this.symbol;
+		
+		if(this.parameters != null) {
+			str += "(";
+			
+			for(var parameter = 0; parameter < this.parameters.length; ++parameter)
+				if(parameter == this.parameters.length - 1)
+					str += this.parameters[parameter] + ")";
+				else
+					str += this.parameters[parameter] + ",";
+		}
+		
+		return str;
+	}
 }
 
 function Lindenmayer() {
@@ -38,7 +100,7 @@ Lindenmayer.prototype = {
 	},
 	
 	process(axiom, iterations) {
-		axiom = axiom.replace(/\s/g, "");
+		var axiom = this.toSymbols(axiom.replace(/\s/g, ""));
 		
 		if(iterations > this.MAX_ITERATIONS)
 			iterations = this.MAX_ITERATIONS;
@@ -49,47 +111,28 @@ Lindenmayer.prototype = {
 		return axiom;
 	},
 	
-	applyRules(sentence) {
+	toSymbols(string) {
 		var symbols = [];
 		
-		for(var index = 0; index < sentence.length; ++index) {
-			var symbol = sentence[index];
-			var parameters = null;
+		for(var index = 0; index < string.length; ++index) {
+			var symbol = new Symbol(string, index);
 			
-			if(index + 1 < sentence.length && sentence[index + 1] == "(") {
-				var start = ++index + 1;
-				while(sentence[++index] != ")");
-				
-				parameters = sentence.substr(start, index - start).split(",");
-			}
-			
-			symbols.push(new Symbol(symbol, parameters));
+			index += symbol.length;
+			symbols.push(symbol);
 		}
 		
-		return this.compileSentence(symbols);
+		return symbols;
 	},
 	
-	compileSentence(symbols) {
+	applyRules(sentence) {
+		return sentence;
+	},
+	
+	toString(symbols) {
 		var sentence = "";
 		
-		for(var index = 0; index < symbols.length; ++index) {
-			var symbol = symbols[index];
-			
-			sentence += symbol.symbol;
-			
-			if(symbol.parameters != null) {
-				sentence += "(";
-				
-				for(var parameter = 0; parameter < symbol.parameters.length; ++parameter) {
-					sentence += symbol.parameters[parameter];
-					
-					if(parameter != symbol.parameters.length - 1)
-						sentence += ",";
-				}
-				
-				sentence += ")";
-			}
-		}
+		for(var index = 0; index < symbols.length; ++index)
+			sentence += symbols[index].print()
 		
 		return sentence;
 	}
